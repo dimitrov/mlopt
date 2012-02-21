0912f40b3368ecd2bd2730fb5165b67630c90afd
Stealth <stealth@skynet-systems.net>
2012-02-21 12:49:29 +0100

    Added license

--- mlopt.py
+++ mlopt.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python2
 
-# Sorts servers for the pacman mirrorlist.  
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# Sorts servers for the pacman mirrorlist. 
+
+# Todo 
+# Sort servers by score, 
+# Sort by speed
+# Query server last_sync, num_checks, check_frequency, cutoff, protocol, completion_pct
 
 
 import urllib2
