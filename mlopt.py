8fb7bb45657d7f8d2f240cb8436602062eed6ea3
Stealth <stealth@skynet-systems.net>
2012-02-25 15:54:24 +0100

    Fixed incomplete server support

--- mlopt.py
+++ mlopt.py
@@ -38,7 +38,12 @@ class Update_ML():
         self.ml_raw = None
         self.ml_sorted = None
         self.ml_servers = {}
-        self.ml_servers_len = 0
+        
+        self.s_total_len = 0
+        self.s_complete_len = 0
+        self.s_incomplete_len = 0
+
+
         self.complete_servers = {}
         self.incomplete_servers = {}
         self.json_stats = None
@@ -47,7 +52,10 @@ class Update_ML():
     def parse_args(self):
         p = argparse.ArgumentParser(description='Mirror list optimizer (mlopt)')
         
-        p.add_argument('--w', dest="write_dest", help="write servers to path")
+        p.add_argument('--w', dest="write_dest", help="write servers to file")
+        
+        p.add_argument('--a', dest="append", action="store_true",
+                       help="append to file")
         
         p.add_argument('--r', dest="read_from", help="read servers from path")
         
@@ -112,18 +120,26 @@ class Update_ML():
                 continue 
 
             else:
-                self.ml_servers[urlparse(line.split()[2])[1]] = line.split()[2]
+                url = urlparse(line.split()[2])
+                self.ml_servers["%s://%s" % (url[0], url[1])] = line.split()[2]
 
-        self.ml_servers_len = len(self.ml_servers)
+
+        self.s_total_len = len(self.ml_servers)
         
-        self.msg("%s servers configured" % (self.ml_servers_len))
-        
-        if self.ml_servers_len == 0:
-            self.msg("No servers are configured")
+        # Tell the user the total
+        if self.s_total_len == 0:
+            print "No servers configured"
             exit()
     
-
+        elif self.s_total_len == 1:
+            self.msg("1 server configured")
         
+        else:
+            self.msg("%s servers configured" % (self.s_total_len))
+        
+
+
+
     def get_stats(self):
         """Gets the remote server statistics"""
 
@@ -136,39 +152,42 @@ class Update_ML():
             print "Could not retrieve statistics, reason: %s" % (e)
             exit(1)
 
+
             
 
     def sort_stats(self):
         """Sorts the gathered statistics"""     
 
+        keys = []
+
+        # Reconstruct the urls from the mirrorlist
+        # so we can compare them later
+        for key in self.ml_servers.keys():
+            a = urlparse(key)
+            keys.append("%s://%s" % (a[0], a[1]))
+            
+        
         for segment in self.json_stats["urls"]:
             
-            segment_url = segment["url"]
-            server_url = urlparse(segment["url"])[1]
+            p_url = urlparse(segment["url"])
+
+            url = "%s://%s" % (p_url[0], p_url[1])
             
-            # Compare the servers from the mirrorlist
-            # to the server stats
-            if server_url in self.ml_servers.keys():
-                
+
+            if url in keys:
                 # Check to see if its complete 
-                if segment["completion_pct"] == 1.0:
-                    
+                if segment["completion_pct"] == 1.0: 
+
                     # If so, store the info 
-                    self.complete_servers[server_url] = [segment, self.ml_servers[server_url]]
+                    self.complete_servers[url] = [segment, self.ml_servers[url]]
+                
                 else:
-                    self.incomplete_servers[server_url] = [segment, self.ml_servers[server_url]]
+                    self.incomplete_servers[url] = [segment, self.ml_servers[url]]
 
-        self.msg("%s out of %s servers are up-to-date" % (len(self.complete_servers), self.ml_servers_len))
+                    
+        self.msg("%s out of %s servers are up-to-date" % (len(self.complete_servers), self.s_total_len))
         
-        if self.args.show_incomplete:
-            print "%s servers incomplete" % (len(self.incomplete_servers))
-            
-            for server in self.incomplete_servers:
-                s = self.incomplete_servers[server]
-                print "percent: %.2f server: %s" % (s["completion_pct"], s["url"])
-
-
-
+        
     def sort_ml(self):
         """Rearranges the server lists"""
 
@@ -181,10 +200,16 @@ class Update_ML():
         final_ml = []
         reverse = []
         
-        for server in self.complete_servers:
+        if self.args.show_incomplete:
+            s_dict = self.incomplete_servers
+
+        else:
+            s_dict = self.complete_servers
+        
+        for server in s_dict:
             # Grabs the method from the complete servers and assigns 
             # server to it, for easy indexing later 
-            temp[self.complete_servers[server][0][self.method]] = server
+            temp[s_dict[server][0][self.method]] = server
 
 
         # Sort the keys, the keys are the *method*
@@ -196,7 +221,7 @@ class Update_ML():
         # Grab the full server from the server dictionary
         # and build a list with it
         for k in c_keys:
-            final_ml.insert(0, self.complete_servers[temp[k]][1])
+            final_ml.insert(0, s_dict[temp[k]][1])
                 
         # Reverse it if needed    
         if self.method in reverse:
@@ -211,25 +236,24 @@ class Update_ML():
         for k in c_keys:
             
             if self.args.write_dest:
-                file_obj.write("Server = %s\n" % (self.complete_servers[temp[k]][1]))
+                file_obj.write("Server = %s\n" % (s_dict[temp[k]][1]))
                                
             else:
-                x = self.complete_servers[temp[k]][0]
+                x = s_dict[temp[k]][0]
                 
-                if self.method == "score":
-                    print "%s: %.2f, server: %s" % (self.method, 
+
+                if self.method == "score" and x[self.method] != None:
+                    print "%s: %.2f  %s" % (self.method, 
                                                     x[self.method],
                                                     x["url"])
                 else:
-                    print "%s: %s, server: %s" % (self.method,
+                    print "%s: %s %s" % (self.method,
                                                   x[self.method],
                                                   x["url"])
-                
                                   
         if self.args.write_dest:
             self.write_ml(file_obj)
                                    
-
         
     def write_ml(self, file_obj):
         """Writes the new mirrorlist to file or stdout"""        
@@ -241,7 +265,17 @@ class Update_ML():
         
         # Otherwise write to file
         else:                
-            with open(self.args.write_dest, "w") as ml:
+            if self.args.append:
+                o_msg  = "Appending"
+                o_mode = "a"
+
+            else:
+                o_msg = "Writting"
+                o_mode = "w"
+
+            self.msg("%s to %s" % (o_msg, self.args.write_dest))
+
+            with open(self.args.write_dest, o_mode) as ml:
                 ml.write(file_obj.getvalue())
                 
         file_obj.close()
